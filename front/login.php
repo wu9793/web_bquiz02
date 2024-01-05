c2065e6444bc5cddc46977d5a4372f34c9582cf5
wu <timmy61501@gmail.com>
2024-01-05 10:57:55 +0800

    登入登出帳號管理介面

--- front/login.php
+++ front/login.php
@@ -2,17 +2,17 @@
     <legend>會員登入</legend>
     <table>
         <tr>
-            <td class="clo">帳號</td>
-            <td><input type="text"></td>
+            <td class='clo'>帳號</td>
+            <td><input type="text" name="acc" id="acc"></td>
         </tr>
         <tr>
-            <td class="clo">密碼</td>
-            <td><input type="password"></td>
+            <td class='clo'>密碼</td>
+            <td><input type="password" name="pw" id="pw"></td>
         </tr>
         <tr>
             <td>
                 <input type="button" value="登入" onclick="login()">
-                <input type="reset" value="清除">
+                <input type="reset" value="清險">
             </td>
             <td>
                 <a href="?do=forget">忘記密碼</a> | <a href="?do=reg">尚未註冊</a>
@@ -21,22 +21,28 @@
     </table>
 </fieldset>
 <script>
-    function login(){
-        $.post('./api/chk_acc.php',{acc:$("#acc").val()},(res)=>{
-            if(parseInt(res)==0){
+    function login() {
+        $.post('./api/chk_acc.php', {
+            acc: $("#acc").val()
+        }, (res) => {
+            if (parseInt(res) == 0) {
                 alert("查無帳號")
-            }else{
-                $.post('./api/chk_pw.php',{acc:$("#acc").val(),pw:$("#pw").val()},(res)=>{
-                    if(parseInt(res)==1){
-                        if($("#acc").val()=='admin'){
-                            location.href= 'back.php'
-                        }else{
-                            location.href='index.php'
+            } else {
+                $.post('./api/chk_pw.php', {
+                        acc: $("#acc").val(),
+                        pw: $("#pw").val()
+                    },
+                    (res) => {
+                        if (parseInt(res) == 1) {
+                            if ($("#acc").val() == 'admin') {
+                                location.href = 'back.php'
+                            } else {
+                                location.href = 'index.php'
+                            }
+                        } else {
+                            alert("密碼錯誤")
                         }
-                    }else{
-                        alert("密碼錯誤")
-                    }
-                })
+                    })
             }
         })
     }
