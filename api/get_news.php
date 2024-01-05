eec3a0019d1cdb3d8e41307e183919cb21d27c79
wu <timmy61501@gmail.com>
2024-01-05 16:35:47 +0800

    調整文章和列表的顯示方式

---
+++ api/get_news.php
@@ -0,0 +1,6 @@
+<?php include_once "db.php";
+
+$news=$News->find($_GET['id']);
+echo nl2br($news['news']);
+
+?>
--- front/po.php
+++ front/po.php
@@ -1,34 +1,60 @@
 <style>
-    .type-item{
+    .type-item {
         display: block;
         margin: 3px 6px;
     }
-    .types , .news-list{
+
+    .types,
+    .news-list {
         display: inline-block;
         vertical-align: top;
     }
-    .news-list{
+
+    .news-list {
         width: 600px;
     }
 </style>
-<div class="nav">目前位置: 首頁 > 分類網誌 > <span class="type">健康新知</span></div>
+<div class="nav">目前位置:首頁 > 分類網誌 > <span class="type">健康新知</span></div>
 
-<fieldset class="types">
+<fieldset class='types'>
     <legend>分類網誌</legend>
-    <a class="type-item">健康新知</a>
-    <a class="type-item">菸害防治</a>
-    <a class="type-item">癌症防治</a>
-    <a class="type-item">慢性病防治</a>
+    <a class='type-item' data-id="1">健康新知</a>
+    <a class='type-item' data-id="2">菸害防治</a>
+    <a class='type-item' data-id="3">癌症防治</a>
+    <a class='type-item' data-id="4">慢性病防治</a>
 </fieldset>
-<fieldset class="news-list">
+<fieldset class='news-list'>
     <legend>文章列表</legend>
-    <div class="list-items"></div>
+    <div class="list-items" style="display:none">
+
+    </div>
     <div class="article"></div>
 </fieldset>
-  
-<script>
-    $(".type-item").on('click',function(){
-        $(".type").text($(this).text())
-    })
-</script>
 
+<script>
+    getList(1)
+    $(".type-item").on('click', function() {
+        $(".type").text($(this).text())
+        let type = $(this).data('id')
+        getList(type)
+
+    })
+
+    function getList(type) {
+        $.get("./api/get_list.php", {
+            type
+        }, (list) => {
+            $(".list-items").html(list)
+            $(".article,.list-items").toggle();
+        })
+    }
+
+    function getNews(id) {
+        $.get("./api/get_news.php", {
+            id
+        }, (news) => {
+            $(".article").html(news)
+            $(".article,.list-items").toggle();
+        })
+    }
+</script>
