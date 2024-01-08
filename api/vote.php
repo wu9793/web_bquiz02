917df7a2821569fb91f8487d4373988ea6331c04
wu <timmy61501@gmail.com>
2024-01-08 16:06:28 +0800

    完成我要投票功能

--- api/vote.php
+++ api/vote.php
@@ -1,15 +1,10 @@
-<?php include_once "db.php";
+<?php include_once 'db.php';
 
-$opt=$Que->find($_POST['opt']);
+$opt = $Que->find($_POST['opt']);
 $opt['vote']++;
 $Que->save($opt);
-$sub=$Que->find(['subject_id']);
-$opt['vote']++;
+$sub = $Que->find($opt['subject_id']);
+$sub['vote']++;
 $Que->save($sub);
 
 to("../index.php?do=result&id={$sub['id']}");
-
-
-
-
-?>
