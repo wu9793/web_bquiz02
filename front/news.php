7c3a8d8a2a66710deb507a880f60da6d31279d91
wu <timmy61501@gmail.com>
2024-01-08 09:55:41 +0800

    人氣文章及最新文章分頁功能

--- front/news.php
+++ front/news.php
@@ -2,14 +2,45 @@
     <legend>目前位置:首頁 > 最新文章區</legend>
     <table style="width: 95%;margin:auto;">
         <tr>
-            <th  width="30%">標題</th>
-            <th  width="50%">內容</th>
+            <th width="30%">標題</th>
+            <th width="50%">內容</th>
             <th></th>
         </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
+        <?php
+        $total = $News->count(['sh' => 1]);
+        $div = 5;
+        $pages = ceil($total / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
+        $rows = $News->all(['sh' => 1], " limit $start,$div");
+        foreach ($rows as $row) {
+        ?>
+            <tr>
+                <td><?= $row['title']; ?></td>
+                <td><?= mb_substr($row['news'], 0, 25); ?>...</td>
+                <td></td>
+            </tr>
+        <?php
+        }
+        ?>
     </table>
+    <div class="ct">
+
+        <?php
+        if ($now > 1) {
+            $prev = $now - 1;
+            echo "<a href='?do=$do&p=$prev'> < </a>";
+        }
+
+        for ($i = 1; $i <= $pages; $i++) {
+            $fontsize = ($now == $i) ? '24px' : '16px';
+            echo "<a href='?do=$do&p=$i' style='font-size:$fontsize'> $i </a>";
+        }
+
+        if ($now < $pages) {
+            $next = $now + 1;
+            echo "<a href='?do=$do&p=$next'> > </a>";
+        }
+        ?>
+    </div>
 </fieldset>
--- front/pop.php
+++ front/pop.php
@@ -6,10 +6,41 @@
             <th width="50%">內容</th>
             <th>人氣</th>
         </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
+        <?php
+        $total = $News->count(['sh' => 1]);
+        $div = 5;
+        $pages = ceil($total / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
+        $rows = $News->all(['sh' => 1], "order by `good` desc");
+        foreach ($rows as $row) {
+        ?>
+            <tr>
+                <td><?= $row['title']; ?></td>
+                <td><?= mb_substr($row['news'], 0, 25); ?>...</td>
+                <td></td>
+            </tr>
+        <?php
+        }
+        ?>
     </table>
+    <div class="ct">
+
+        <?php
+        if ($now > 1) {
+            $prev = $now - 1;
+            echo "<a href='?do=$do&p=$prev'> < </a>";
+        }
+
+        for ($i = 1; $i <= $pages; $i++) {
+            $fontsize = ($now == $i) ? '24px' : '16px';
+            echo "<a href='?do=$do&p=$i' style='font-size:$fontsize'> $i </a>";
+        }
+
+        if ($now < $pages) {
+            $next = $now + 1;
+            echo "<a href='?do=$do&p=$next'> > </a>";
+        }
+        ?>
+    </div>
 </fieldset>
