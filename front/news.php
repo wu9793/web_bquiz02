437d7d78a4672fc9778bf8622ccc7bc5690efffa
wu <timmy61501@gmail.com>
2024-01-08 10:57:23 +0800

    完成最新文章顯示完整文章功能

--- front/news.php
+++ front/news.php
@@ -12,12 +12,16 @@
         $pages = ceil($total / $div);
         $now = $_GET['p'] ?? 1;
         $start = ($now - 1) * $div;
+
         $rows = $News->all(['sh' => 1], " limit $start,$div");
         foreach ($rows as $row) {
         ?>
             <tr>
-                <td><?= $row['title']; ?></td>
-                <td><?= mb_substr($row['news'], 0, 25); ?>...</td>
+                <td><div class="title" data-id="<?=$row['id'];?>" style="cursor:pointer"><?= $row['title']; ?></div></td>
+                <td>
+                    <div id="s<?=$row['id']?>"><?= mb_substr($row['news'], 0, 25); ?>...</div>
+                    <div id="a<?=$row['id']?>"><?=$row['news']?></div>
+                </td>
                 <td></td>
             </tr>
         <?php
@@ -44,3 +48,12 @@
         ?>
     </div>
 </fieldset>
+<script>
+    $(".title").on('click',(e)=>{
+        let id=$(e.target).data('id');
+        $(`#s${id},#a${id}`).toggle();
+
+    })
+
+
+</script>
